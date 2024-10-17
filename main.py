a0641e147bea5c36f7021b613c5687aa88e545e2
ReyCarlosV <carlostes02@gmail.com>
2024-10-17 14:55:12 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from classWINDOW import Window
+
+ventana = Window()
+
+ventana.set_dimensions(500, 500)
+ventana.set_title("mi primera App")
+ventana.set_resize(False)
+
+#print(ventana.get_dimensions())
+#print(ventana.get_resize())
+#print(ventana.get_title())
+
+ventana.crear_ventana()
